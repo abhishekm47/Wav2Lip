a71e2828faa5a59d81967ff80ed5369e4766b7c1
Abhishek meena <41361518+abhishekm47@users.noreply.github.com>
2021-04-20 13:33:37 +0530

    Update inference.py
    code beautify and comments added

--- inference.py
+++ inference.py
@@ -10,28 +10,40 @@ import app.api.Wav2Lip.face_detection as face_detection
 from app.api.Wav2Lip.models import Wav2Lip
 import platform
 
+#Name of saved checkpoint to load weights from
 checkpoint_path = "/home/ubuntu/FaceChangeBabbleAPIAdmin/app/api/Wav2Lip/checkpoints/wav2lip.pth"
 
+#If True, then use only first video frame for inference
 static = False
 
+#Can be specified only if input is a static image (default: 25)
 final_fps = 25.
 
+#Padding (top, bottom, left, right). Please adjust to include chin at least
 bbox_pads = [0, 10, 0, 0]
 
+#Batch size for face detection
 face_det_batch_size = 16
 
+#Batch size for Wav2Lip model(s)
 wav2lip_batch_size = 128
 
+#Reduce the resolution by this factor. Sometimes, best results are obtained at 480p or 720p
 resize_factor = 1
 
+#Crop video to a smaller region (top, bottom, left, right). Applied after resize_factor and rotate arg. Useful if multiple face present. -1 implies the value will be auto-inferred based on height, width
 crop = [0, -1, 0, -1]
 
+#Specify a constant bounding box for the face. Use only as a last resort if the face is not detected. Also, might work only if the face is not moving around much. Syntax: (top, bottom, left, right).
 submit_box = [-1, -1, -1, -1]
 
+#Sometimes videos taken from a phone can be flipped 90deg. If true, will flip video right by 90deg.Use if you get a flipped result, despite feeding a normal looking videoSometimes videos taken from a phone can be flipped 90deg. If true, will flip video right by 90deg. Use if you get a flipped result, despite feeding a normal looking video
 rotate = False
 
+#Prevent smoothing face detections over a short temporal window
 nosmooth = False 
 
+#image size
 final_img_size = 96
 
 
@@ -158,6 +170,12 @@ def load_model(path):
 	model = model.to(device)
 	return model.eval()
 
+#run wav2lip infrence
+"
+face_file: Filepath of video/image that contains faces to use,
+audio_file:Filepath of video/audio file to use as raw audio source,
+outfile:Video path to save result
+"
 def wav2lip_inference_modify(face_file, audio_file, outfile):
 	if not os.path.isfile(face_file):
 		raise ValueError('--face argument must be a valid path to video/image file')
